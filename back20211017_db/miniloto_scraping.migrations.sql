6ac4f097aee1feeee645b64e0d096ad6004696a8
Hirofumi Sameshima <jawssame7@gmail.com>
2021-10-17 10:25:12 +0900

    db backup

---
+++ back20211017_db/miniloto_scraping.migrations.sql
@@ -0,0 +1,14 @@
+insert into miniloto_scraping.migrations(id,migration,batch) values 
+ (1,'2014_10_12_000000_create_users_table',1)
+,(2,'2014_10_12_100000_create_password_resets_table',1)
+,(3,'2019_08_19_000000_create_failed_jobs_table',1)
+,(4,'2019_12_14_000001_create_personal_access_tokens_table',1)
+,(5,'2021_08_22_030413_create_miniloto_result',1)
+,(6,'2021_08_22_064555_modefied_column_miniloto_scraping',2)
+,(7,'2021_08_22_082107_rename_miniloto_result_to_miniloto_results',3)
+,(8,'2021_08_22_101929_create_miniloto_past_urls',4)
+,(9,'2021_09_18_050341_add_column_miniloto_past_usls',5)
+,(10,'2021_09_19_060130_add_column_miniloto_past_urls',6)
+,(11,'2021_09_25_061313_create_miniloto_forecasts',7)
+,(12,'2021_09_26_031250_modified_miniloto_forecasts',8)
+;
