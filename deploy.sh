fd670ad11493567af35a888225ad7199b6966380
Hirofumi Sameshima <jawssame7@gmail.com>
2021-10-30 14:56:26 +0900

    github actions modified

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # srcファイルをrsync over SSHで転送
-rsync -acvzrptgDve --delete ./ web@ik1-344-32290.vs.sakura.ne.jp:miniloto-scraping/
+rsync -acvzrptgDve --delete ./ web@ik1-344-32290.vs.sakura.ne.jp:/home/web/test
 # サービスを再起動
 # ssh web@ik1-344-32290.vs.sakura.ne.jp "cd miniloto-scraping/; docker-compose build; docker-compose down; docker-compose -f docker-compose.yml -f docker-compose-prod.yml up -d"
