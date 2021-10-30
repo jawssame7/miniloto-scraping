3cc5f196e35abb2e9c0bf8c855a6ac7ca05fd3b5
Hirofumi Sameshima <jawssame7@gmail.com>
2021-10-30 16:32:33 +0900

    depoy sh 変更

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # srcファイルをrsync over SSHで転送
-rsync -acvz --delete -e "ssh -i key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" ./ web@ik1-344-32290.vs.sakura.ne.jp:/home/web/miniloto/
+rsync -acvz --delete -e "ssh -i key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" ./ web@ik1-344-32290.vs.sakura.ne.jp:/home/web/workspace/miniloto/
 # サービスを再起動
-# ssh web@ik1-344-32290.vs.sakura.ne.jp "cd miniloto-scraping/; docker-compose build; docker-compose down; docker-compose -f docker-compose.yml -f docker-compose-prod.yml up -d"
+ssh web@ik1-344-32290.vs.sakura.ne.jp "cd /home/web/workspace/miniloto/; docker-compose build; docker-compose down; docker-compose -f docker-compose-prod.yml up -d"
